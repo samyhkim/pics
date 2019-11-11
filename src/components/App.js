a811155e1fe3da19a4b0cbd70ada1ccfd49c4090
Samuel Kim <samuelkim@Samuels-MacBook-Pro.local>
2019-11-11 11:31:57 -0800

    Connected to Unsplash API using axios.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID 36e342ae227212bd189844e32a78a10674dc2e9a7e1f831b620188ebdb87fdc2"
+      }
+    });
   }
 
   render() {
