79b17715318d46042168c5ed72b2e4875c9ccdd5
Samuel Kim <samuelkim@Samuels-MacBook-Pro.local>
2019-11-11 13:03:29 -0800

    Mapped images to return image element. Our app now displays images fetched from Unsplash.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-import SearchBar from "./SearchBar";
 import unsplash from "../api/unsplash";
+import SearchBar from "./SearchBar";
+import ImageList from "./ImageList";
 
 class App extends React.Component {
   state = { images: [] };
@@ -8,7 +9,6 @@ class App extends React.Component {
     const response = await unsplash.get("/search/photos", {
       params: { query: term }
     });
-    console.log(this);
     this.setState({ images: response.data.results });
   };
 
@@ -16,7 +16,7 @@ class App extends React.Component {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmitProp={this.onSearchSubmit} />
-        Found: {this.state.images.length} images
+        <ImageList images={this.state.images} />
       </div>
     );
   }
